baca1b204af11b056a8283ba5aa1cc11405f8ee2
WAJDIMADANI <86354825+WAJDIMADANI@users.noreply.github.com>
2026-02-03 11:44:11 +0100

    Updated ACTIVER-PERMISSION-SMS-MAINTENANT.sql

--- ACTIVER-PERMISSION-SMS-MAINTENANT.sql
+++ ACTIVER-PERMISSION-SMS-MAINTENANT.sql
@@ -6,16 +6,17 @@
 */
 
 -- Ajouter la permission rh/sms à tous les utilisateurs qui ont déjà rh/emails
-INSERT INTO permission_utilisateur (utilisateur_id, permission)
-SELECT DISTINCT utilisateur_id, 'rh/sms'
-FROM permission_utilisateur
-WHERE permission = 'rh/emails'
+INSERT INTO utilisateur_permissions (utilisateur_id, section_id, actif)
+SELECT DISTINCT utilisateur_id, 'rh/sms', true
+FROM utilisateur_permissions
+WHERE section_id = 'rh/emails'
+  AND actif = true
   AND NOT EXISTS (
-    SELECT 1 FROM permission_utilisateur pu2
-    WHERE pu2.utilisateur_id = permission_utilisateur.utilisateur_id
-    AND pu2.permission = 'rh/sms'
+    SELECT 1 FROM utilisateur_permissions up2
+    WHERE up2.utilisateur_id = utilisateur_permissions.utilisateur_id
+    AND up2.section_id = 'rh/sms'
   );
 
 -- OU pour un utilisateur spécifique, remplacez 'ID-UTILISATEUR' par l'ID de l'utilisateur
--- INSERT INTO permission_utilisateur (utilisateur_id, permission)
--- VALUES ('ID-UTILISATEUR', 'rh/sms');
+-- INSERT INTO utilisateur_permissions (utilisateur_id, section_id, actif)
+-- VALUES ('ID-UTILISATEUR', 'rh/sms', true);
