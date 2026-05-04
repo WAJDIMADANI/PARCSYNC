cd9783ebbdb44aef541ae93e2d021817d2330ab3
WAJDIMADANI <86354825+WAJDIMADANI@users.noreply.github.com>
2026-05-04 12:18:36 +0100

    Updated PaiementsManager.tsx

--- src/components/PaiementsManager.tsx
+++ src/components/PaiementsManager.tsx
@@ -495,7 +495,7 @@ export function PaiementsManager() {
         </div>
       )}
 
-     {/* Modal de pointage */}
+{/* Modal de pointage */}
       {pointageId && paiementPointage && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
           <div className="bg-white rounded-lg shadow-xl max-w-md w-full p-6 max-h-[90vh] overflow-y-auto">
@@ -599,4 +599,6 @@ export function PaiementsManager() {
           </div>
         </div>
       )}
+    </div>
+  );
 }
